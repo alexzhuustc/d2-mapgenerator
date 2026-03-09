bce822de51106c379fd8f86fe2111fdda69735c7
Alex <alexzhu.ustc@gmail.com>
2026-03-09 21:46:09 +0800

    create release.

---
+++ release/blacha_mapgen.bat
@@ -0,0 +1,5 @@
+REM D2 LOD Dir = game
+REM Seed = 1933916770
+REM Act6 = Black Marsh
+REM Difficulty2 = Hell
+d2-mapgen game --seed 1933916770 --map 6 --difficulty 2
---
+++ release/joffreybesos_mapgenerator.bat
@@ -0,0 +1,5 @@
+REM D2 LOD Dir = game
+REM Seed = 1933916770
+REM Act6 = Black Marsh
+REM Difficulty2 = Hell
+d2-mapgenerator.exe generate -b d2-mapgen.exe --d2lod game --seed 1933916770 --map 6 --difficulty 2
